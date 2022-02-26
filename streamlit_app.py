ae0f76dbcd9aec8ea9d65e9e1eca6184376b99b5
Grace Luo <graceluo@berkeley.edu>
2022-02-26 13:52:41 -0800

    Ask user to input Aleph investigation instead of browsing

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,15 +79,15 @@ def process_mhtml(zipf, fname):
   with open("page.html", "wb") as fd:
     fd.write(html_body)
   response = upload_files("page.html", meta)
-  os.path.remove("page.html")
+  os.remove("page.html")
 
 def process_hunchly(hunchly_export):
   zipf = zipfile.ZipFile(hunchly_export)
   filelist = zipf.namelist()
+  filelist = [fname for fname in filelist if fname.endswith(".mhtml")]
 
   for fname in stqdm(filelist):
-    if fname.endswith(".mhtml"):
-      process_mhtml(zipf, fname)
+    process_mhtml(zipf, fname)
 
 # ===========================================
 #             Streamlit GUI
@@ -99,12 +99,13 @@ def show_streamlit():
     **How-To Guide:**
     This web GUI takes your [Hunchly](https://www.hunch.ly) case and uploads it to 
     [OCCRP Aleph](https://aleph.occrp.org/). Note that it only uploads mhtml files and does NOT process
-    images or attachments. Export your Hunchly case, upload the zip file, input your Aleph API Key, and get the Aleph investigation 
+    images or attachments. Export your Hunchly case, upload the zip file, input your Aleph API Key, and link the Aleph investigation 
     to upload to. Finally, click Submit and wait for the tool to upload all files.
 
     - To export your Hunchly Case: Open Hunchly, go to Export, and click "Export Case".
     - To get your Aleph API Key: Go to [Settings](https://aleph.occrp.org/settings), and copy "API Secret Access Key".
-    - To create a new Aleph Investigation: Select "Create New Investigation" and inputting a name.
+    - To link a Aleph Investigation: Go to [Investigations](https://aleph.occrp.org/investigations), select one, and input the link that appears in the top bar. \
+    To create a new investigation, input a new name instead of a link.
 
     Made with ❤️ in Berkeley by the [Berkeley Investigative Reporting Program](https://journalism.berkeley.edu/programs/mj/investigative-reporting/) (with source code
     from the Hunchly team).
@@ -121,27 +122,17 @@ def show_streamlit():
     accept_multiple_files=False,
   )
 
-  # Browse Aleph Investigations
-  aleph_investigations = ["Create New Investigation"]
-
   if "option" not in st.session_state:
-    st.session_state.investigation = aleph_investigations[0]
-    st.session_state.new_investigation = ""
+    st.session_state.investigation = ""
     st.session_state.collection_id = 0
     st.session_state.api_key = ""
 
   st.session_state.api_key = st.text_input("Input Aleph API Key")
-  st.session_state.investigation = st.selectbox(
-    "Browse Aleph Investigations",
-    aleph_investigations,
-  )
 
   # Disable if not creating new investigation
-  create_new_investigation = st.session_state.investigation == aleph_investigations[-1]
-  st.session_state.new_investigation = st.text_input(
-    "Name Aleph Investigation",
-    placeholder=st.session_state.investigation if not create_new_investigation else "", 
-    disabled=(not create_new_investigation)
+  st.session_state.investigation = st.text_input(
+    "Link Aleph Investigation",
+    placeholder="https://aleph.occrp.org/investigations/0 OR New Investigation Name"
   )
   
   # Disable if no Hunchly zip or new investigation name is blank
@@ -149,14 +140,15 @@ def show_streamlit():
     label="Submit",
     disabled=(not st.session_state.hunchly_export) or \
       (not st.session_state.api_key) or \
-      (create_new_investigation and not st.session_state.new_investigation)
+      (not st.session_state.investigation)
   )
   
-  # TODO: get collections
   if submitted:
-    if st.session_state.new_investigation:
-      response = create_collection(st.session_state.new_investigation)
+    if "aleph.occrp.org" not in st.session_state.investigation:
+      response = create_collection(st.session_state.investigation)
       st.session_state.collection_id = response["collection_id"]
+    else:
+      st.session_state.collection_id = st.session_state.investigation.split("/")[-1]
     process_hunchly(st.session_state.hunchly_export)
     st.success("Success! Uploaded all HTML files to Aleph.")
 
